e21c8b45dccb9375a07c2acb38627297e377a18d
BloodShop <koliakovcr7@gmail.com>
2022-12-13 17:23:51 +0200

    !

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,3 @@ AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000
 });
-
-// Scroll to specific values
-// window.scrollTo or window.scroll
-window.scroll({
-  top: 1000,
-  left: 0,
-  behavior: 'smooth'
-});
-
-// Scroll certain amounts from current position
-window.scrollBy({
-  top: 250, // could be negative value
-  left: 0,
-  behavior: 'smooth'
-});
